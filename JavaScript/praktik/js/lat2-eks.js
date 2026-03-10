d50458105ee431cf2b56885420fe3397bf8920a3
Kherilirli <januarkherilirli@gmail.com>
2026-03-10 13:46:34 +0700

    Tugas-JavaScript-1

---
+++ JavaScript/praktik/js/lat2-eks.js
@@ -0,0 +1,4 @@
+alert("Selamat datang di website saya...");
+
+document.write("<h1>WEBSITE SAYA</h1>"); //Output akan ditampilkan di halaman web
+        console.log("Alamat Saya di Bogor"); //Output akan ditampilkan di console 
